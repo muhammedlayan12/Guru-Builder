b3438fdb375090d782f17a43668cf024d07bfe48
muhammedlayan12 <muhammedlayan12@gmail.com>
2024-12-17 19:35:56 +0500

    Its My First Client

--- js/script.js
+++ js/script.js
@@ -52,3 +52,14 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     //     alert("Hello");
     // });
  
+
+
+    //js for video 
+    const video = document.getElementById("myVideo");
+    video.onclick = function() {
+        if (video.paused) {
+            video.play(); 
+        } else {
+            video.pause();  
+        }
+    };
